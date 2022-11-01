28031cdd2905794bffbc7d5798db122803728244
HayaUp <hayaup.git@gmail.com>
2022-11-02 00:58:40 +0900

    [update]定型文の第2階層をselectで選択できるようにした。

--- sample/main.js
+++ sample/main.js
@@ -1,8 +1,24 @@
 "use strict";
 
-window.addEventListener(
-    "DOMContentLoaded",
-    () => {
-        const fpg1 = new FixedPhraseGroup1();
-    }
-);
+window.addEventListener("DOMContentLoaded", () => {
+    const fpg1 = new FixedPhraseGroup1();
+    const fpg2 = new FixedPhraseGroup2();
+
+    const fpg1_element = document.getElementById("FixedPhraseGroup1");
+    const fpg2_element = document.getElementById("FixedPhraseGroup2");
+
+    fpg1_element.addEventListener("change", (e) => {
+        fpg2_element.disabled = e.target.value == 0;
+        fpg2_element.value = 0;
+
+        for(let i = 0; i < hoge2.options.length; i++) {
+            const item = hoge2.options[i];
+
+            if(item.dataset.group1_id == e.target.value) {
+                item.style.display = "block"
+            }
+            else {
+                item.style.display = "none";
+            }
+        }
+    });});
